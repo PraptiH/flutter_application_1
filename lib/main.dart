92456e8d857c78261c90f37f114243cc660129ed
PraptiH <praptiantara1892@gmail.com>
2025-02-18 20:50:22 +0600

    3rd task

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,87 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Basic flutter UI')),
-        body: Container(
-          child: Row(
+      title: 'Fancy Icon App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Shoe widget'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Icon(
-                Icons.star,
-                size: 60,
-                color: const Color.fromARGB(255, 0, 0, 0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Text(
+                    '50\% OFF',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Icon(Icons.favorite)
+                ],
               ),
-              Icon(
-                Icons.star,
-                size: 60,
-                color: const Color.fromARGB(255, 0, 0, 0),
+              const SizedBox(height: 20),
+              Image.network(
+                'https://tinyurl.com/cse414-nike-res',
+                height: 300,
+                width: 300,
               ),
-              Icon(
-                Icons.star,
-                size: 60,
-                color: const Color.fromARGB(255, 0, 0, 0),
+              const SizedBox(height: 20),
+              const Text(
+                'Nike Air Shoes',
+                style: TextStyle(fontSize: 18),
               ),
-              Icon(
-                Icons.star,
-                size: 60,
-                color: const Color.fromARGB(255, 0, 0, 0),
-              ),
-              Icon(
-                Icons.star,
-                size: 60,
-                color: const Color.fromARGB(255, 30, 27, 27),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Text(
+                    '\$45,000  ',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    '\$55,000',
+                    style: TextStyle(
+                      fontSize: 16,
+                      decoration: TextDecoration.lineThrough,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
         ),
       ),
-    );
-  }
-}
-
-class MyWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      'Hello, Flutter!',
-      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
     );
   }
 }
